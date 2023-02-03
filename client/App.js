8528c1065ec27e207634d7de9700c93f2149e6f5
Andrew Choi <andrewsungwoochoi@gmail.com>
2023-02-03 12:43:57 -0800

    added user controllers and app.js routing

--- client/App.js
+++ client/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Inventory from './components/Inventory';
+import Navbar from './components/Navbar';
+import Login from './components/Login'
+import Register from './components/Register';
+import Reset from './components/Reset';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Routes>
+      <>
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/reset" element={<Reset />} />
+        <Route exact path="/dashboard" element={<Dashboard />} />
+        <Route exact path="/inventory" element={<Inventory />} />
+      </>
+      </Routes>
+    </Router>
   );
 }
 
